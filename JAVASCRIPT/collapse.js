f488b99cf8ba8cf8b87844ae2331d3d61ed428ba
2351879-Ricardo <2351879@students.wits.ac.za>
2022-06-10 13:29:34 +0200

    Update collapse.js

--- JAVASCRIPT/collapse.js
+++ JAVASCRIPT/collapse.js
@@ -1,6 +1,6 @@
 function ToggleCollapseView(element)
 {
-    var content = element.parentNode.nextElementSibling;
+    let content = element.parentNode.nextElementSibling;
     if (content.style.height === "inherit"){
         content.style.height = 0;
         content.style.opacity = 0;}
