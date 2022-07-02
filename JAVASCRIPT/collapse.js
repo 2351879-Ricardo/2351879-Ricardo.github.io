2ac2d0c7f42c15808010e9a1cc9bc18f593355e6
2351879-Ricardo <2351879@students.wits.ac.za>
2022-07-02 12:27:11 +0200

    Revamp >> Main Overwrite

---
+++ JAVASCRIPT/collapse.js
@@ -0,0 +1,11 @@
+function ToggleCollapseView(element)
+{
+    let content = element.parentNode.nextElementSibling;
+    if (content.style.height === "inherit"){
+        content.style.height = 0;
+        content.style.opacity = 0;}
+    else{
+        content.style.height = "inherit";
+        content.style.opacity = 1;
+    }
+}
---
+++ JAVASCRIPT/import_templates.js
@@ -0,0 +1,48 @@
+function ImportNavBar(id)
+{
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function()
+        {
+            if (this.readyState == 4)
+            {
+                if (this.status == 200) 
+                {
+                    document.getElementById("nav-bar").innerHTML = this.responseText;
+                    HighlightCurrent(id);
+                }
+            }
+        };
+
+    xhttp.open("GET", "../Templates/navbar.html", true);
+    xhttp.send();
+
+    return;
+}
+
+function HighlightCurrent(id)
+{
+    let elemStyle = document.getElementById(id).style;
+    elemStyle.textTransform = "uppercase";
+    elemStyle.fontVariant = "normal";
+    elemStyle.color = "white";
+    elemStyle.backgroundColor = "rgba(28, 15, 203, 0.70)";
+
+}
+
+function ImportBlog(blog)
+{    
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function()
+    {
+        if (this.readyState == 4)
+        {
+            if (this.status == 200) {document.getElementById("blog-container").innerHTML += this.responseText;}
+        }
+    }
+
+    xhttp.open("GET", blog, true);
+    xhttp.send();  
+      
+    return;
+}
